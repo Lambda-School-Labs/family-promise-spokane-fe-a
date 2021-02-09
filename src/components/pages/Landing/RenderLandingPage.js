80662f0dab2f69a367b96644d58d94a2c0227db0
mbenson3434 <44735865+mbenson3434@users.noreply.github.com>
2021-02-09 12:10:52 -0500

    Changed LoginContainer.js line 23 to read welcome to Family Promise

--- src/components/pages/Landing/RenderLandingPage.js
+++ src/components/pages/Landing/RenderLandingPage.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 function RenderLandingPage(props) {
   return (
     <div>
-      <h1>Welcome to Labs Basic SPA</h1>
+      <h1>Welcome to Family Promise</h1>
       <div>
         <p>
           This is an example of how we'd like for you to approach page/routable
