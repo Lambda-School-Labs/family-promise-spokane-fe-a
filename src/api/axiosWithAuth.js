8b90d231cb8fcc46d4fc1eed63b5616369098e57
mbenson3434 <44735865+mbenson3434@users.noreply.github.com>
2021-02-15 18:13:43 -0500

    reverting back to deployed back end

--- src/api/axiosWithAuth.js
+++ src/api/axiosWithAuth.js
@@ -1,15 +1,15 @@
 import axios from 'axios';
 const environment = process.env.ENV || 'development';
 
-environment == 'development'
-  ? console.log('development mode')
-  : console.log('something else');
+// environment !== 'development'
+//   ? console.log('development mode')
+//   : console.log('something else');
 
-// for testing BE on localhost
-const ApiUrl =
-  environment !== 'development'
-    ? process.env.REACT_APP_API_HEROKU
-    : process.env.REACT_APP_API_URI;
+// // for testing BE on localhost
+// const ApiUrl =
+//   environment !== 'development'
+//     ? process.env.REACT_APP_API_HEROKU
+//     : process.env.REACT_APP_API_URI;
 
 export const axiosWithAuth = () => {
   const token = JSON.parse(localStorage.getItem('okta-token-storage'))?.idToken
@@ -18,6 +18,7 @@ export const axiosWithAuth = () => {
     headers: {
       Authorization: `Bearer ${token}`,
     },
-    baseURL: ApiUrl,
+    baseURL: 'https://family-pomise-spokane.herokuapp.com',
+    //baseURL: ApiUrl,
   });
 };
